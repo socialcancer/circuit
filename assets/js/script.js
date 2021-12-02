5ee952734b8083b61bfef00d58ba5896f10cb3e3
Connor White <connorwhitepdx@gmail.com>
2021-12-01 22:06:07 -0800

    updating html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var tasks = {};
 var createTask = function (taskText, taskDate, taskList) {
     // create elements that make up a task item
     var taskLi = $("<li>").addClass("list-group-item");
-
+    
     var taskSpan = $("<span>")
         .addClass("badge badge-primary badge-pill")
         .text(taskDate);
