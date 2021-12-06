390b25bd3b0c21dda6e3d99700dddf864d10bff1
Connor White <connorwhitepdx@gmail.com>
2021-12-05 20:04:02 -0800

    updated html and css

--- assets/js/circuit.js
+++ assets/js/circuit.js
@@ -2,12 +2,32 @@ $(document).ready(function(){
     $('.modal').modal();
   });
 
+$(document).ready(function(){
+  $('.parallax').parallax();
+ });
+
+var addExercise = function(exerciseName) {
+
+    //create list item
+    var exerciseLi = $("<li>").addClass("exercise-item");
+
+    var durationSpan = $("<span>")
+      .text(exerciseDuration);
+
+    //create p element for exercise name
+    var exerciseP = $("<p>")
+        .text(exerciseName);
+
+    //append p element to li    
+    exerciseLi.append(exerciseName);
+
+    //append li to ul
+    $("#exercise-list").append(exerciseLi);
+
+
+
+};
+
 var exerciseName = $("#exercise").val();
 
 var exerciseDuration = $("#duration").val();
-
-var addExercise = $("#addButton").click(function(){
-
-  $("#exercise-list").append('<li>' +exerciseName+ '</li>');
-  
-});
